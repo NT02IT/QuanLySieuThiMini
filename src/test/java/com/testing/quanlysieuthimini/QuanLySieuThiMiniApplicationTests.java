3b0b937f2fab325bda0aeb6600ea06285876818a
Low Tee <lowtee.vn@gmail.com>
2024-12-03 11:06:09 +0700

    init

---
+++ src/test/java/com/testing/quanlysieuthimini/QuanLySieuThiMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.quanlysieuthimini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLySieuThiMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
